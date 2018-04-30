18689a5bda9e503998e1ba95f32f768196dc6963
chasekregor <chase.kregor@colorado.edu>
2018-04-29 20:11:46 -0600

    built website with working predictions

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
+from sklearn.cluster import KMeans
 
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
@@ -12,52 +14,66 @@ def home():
 
 	pkl_file = open('movies', 'rb')
 	movies = pickle.load(pkl_file)
+	sampledmovies = np.random.choice(movies,500)
 
 
 
 
-	return render_template('home.html', movies = movies)
+	return render_template('home.html', movies = sampledmovies)
 
-@app.route("/getresult",methods=['POST','GET'])
+@app.route("/result",methods=['POST','GET'])
 def get_result():
+
 	if request.method=='POST':
 		result=request.form
-		day_of_week = result['day_of_week']
+		resultdict = dict(result)
+		resulttitles=list(resultdict.keys())
 
-		pkl_file = open('cat', 'rb')
-		index_dict = pickle.load(pkl_file)
-		cat_vector = np.zeros(len(index_dict))
+
 
 
 		pkl_file = open('kmeans.pkl', 'rb')
 		kmeansmodel = pickle.load(pkl_file)
 
-		try:
-			cat_vector[index_dict['DAY_OF_WEEK_'+str(day_of_week)]] = 1
-		except:
-			pass
+		pkl_file = open('movies', 'rb')
+		movies = pickle.load(pkl_file)
+
+		predlist = []
 
 
-		usercluster = kmeansmodel.predict(cat_vector)
-		allratings = kmeans.cluster_centers_[userscluster]
+		for c, i in enumerate(movies):
+			if i in resulttitles:
+				predlist.append(resultdict[i])
+			else:
+				predlist.append(movies[c]["average"])
 
-		sort_index = np.argsort(allratings)
-		top20 = sort_index[-21:-1]
 
-		movietitles = kmeansmodel.columns
+		usercluster = kmeansmodel.predict([predlist])
+		allratings = kmeansmodel.cluster_centers_[usercluster]
+
+		sort_index = np.argsort(allratings[0])
+		print(sort_index)
+		print(allratings)
+		top20 = sort_index[-31:-1]
+		print(top20)
 
 		prediction = []
 
 		for i in top20:
-			prediction.append(movietitles[i])
+			prediction.append(movies[i])
 
+		print(prediction)
 
-		return render_template('result.html',prediction=prediction)
+	return render_template('result.html',prediction=prediction, movies = movies)
 
 
 @app.route("/about")
 def about():
 	return render_template('about.html')
+
+@app.route("/howitworks")
+def howitworks():
+	return render_template('howitworks.html')
 
 if __name__ == '__main__':
 	app.debug = True
