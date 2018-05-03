ddeb0d14449a9e3cd93cb55720c621d76fda5b9f
chasekregor <chase.kregor@colorado.edu>
2018-05-03 12:42:55 -0600

    updated web application

--- flask-app/app.py
+++ flask-app/app.py
@@ -105,7 +105,7 @@ def get_result():
 		for i in top30:
 			prediction.append(movies[i])
 
-		return render_template('result.html',prediction=prediction, movies = movies)
+		return render_template('result.html',prediction=prediction, movies = movies, usercluster = usercluster)
 
 """
 		for ix, moviedict in enumerate(movies):
